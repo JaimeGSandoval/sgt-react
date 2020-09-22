3890264f6210f9d0d1454f84f84e9e1ed52635e0
jaimegsandoval <jaime.devs@gmail.com>
2020-09-21 17:19:35 -0700

    Grade component functionality added to GradeTable compponent

--- client/components/grade-table.jsx
+++ client/components/grade-table.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Grade(props) {
   return (
     <tr>
-      <td>props.name</td>
-      <td>props.course</td>
-      <td>props.grade</td>
+      <td>{props.name}</td>
+      <td>{props.course}</td>
+      <td>{props.grade}</td>
     </tr>
   );
 }
@@ -31,21 +31,10 @@ function GradeTable(props) {
           <tbody>
             {
               props.grades.map(grade => {
-                return <Grade key={grade.id} name={grade.name} course={grade.course} />;
+                return <Grade key={grade.id} name={grade.name} course={grade.course} grade={grade.grade} />;
               })
 
             }
-            {/* {
-              props.todos.map(todo => {
-                return (
-                  <Todo
-                    key={todo.id}
-                    todo={todo}
-                    toggleCompleted={props.toggleCompleted} />
-                );
-              })
-            } */}
-            {/* <Grade grade={props.grades} /> */}
           </tbody>
         </table>
         <p className={noGradesText}>No Grades Recorded</p>
@@ -54,39 +43,5 @@ function GradeTable(props) {
   );
 
 }
-
-// class GradeTable extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { grades: [] };
-//   }
-
-//   render() {
-//     let noGradesText = 'noGradesText d-none';
-//     if (!this.state.grades) {
-//       noGradesText = 'noGradesText';
-//     }
-
-//     return (
-//       <div className="row" >
-//         <div className="col-12">
-//           <table className="table">
-//             <thead className="bg-secondary">
-//               <tr>
-//                 <th scope="col-6" className="py-3">Student Name</th>
-//                 <th scope="col-3" className="py-3">Course</th>
-//                 <th scope="col-3" className="py-3">Grade</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-
-//             </tbody>
-//           </table>
-//           <p className={noGradesText}>No Grades Recorded</p>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 export default GradeTable;

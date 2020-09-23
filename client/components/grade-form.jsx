f0d2497c1167aa48ab2c22ed6b772ec44b8e0c29
jaimegsandoval <jaime.devs@gmail.com>
2020-09-22 17:19:30 -0700

    Refactored. Removed the InputField component and return just one large GradeForm component

--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -1,38 +1,5 @@
 import React from 'react';
 
-function InputFields(props) {
-
-  return (
-    <>
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text"><i className="fas fa-user"></i></span>
-        </div>
-        <input type="text" className="form-control" onChange={props.onChange} placeholder="Name" name="name" aria-label="Name" />
-      </div>
-
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text"><i className="fas fa-table"></i></span>
-        </div>
-        <input type="text" className="form-control" onChange={props.onChange} placeholder="Course" name="course" aria-label="Course" />
-      </div>
-
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text"><i className="fas fa-graduation-cap"></i></span>
-        </div>
-        <input type="text" className="form-control" onChange={props.onChange} placeholder="Grade" name="grade" aria-label="Grade" />
-      </div>
-      <div className="btn-box justify-content-end d-flex">
-        <button className="btn-success pb-1" type="submit">Add</button>
-        <button className="btn-success ml-2 pb-1" onClick={props.resetForm} type="reset">Cancel</button>
-      </div>
-    </>
-  );
-
-}
-
 class GradeForm extends React.Component {
   constructor(props) {
     super(props);
@@ -78,7 +45,30 @@ class GradeForm extends React.Component {
       <div className="col-4">
         <form action="/api/grades" onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <InputFields resetForm={this.resetForm} onChange={this.handleChange} />
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text"><i className="fas fa-user"></i></span>
+              </div>
+              <input type="text" className="form-control" onChange={this.handleChange} placeholder="Name" name="name" aria-label="Name" />
+            </div>
+
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text"><i className="fas fa-table"></i></span>
+              </div>
+              <input type="text" className="form-control" onChange={this.handleChange} placeholder="Course" name="course" aria-label="Course" />
+            </div>
+
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text"><i className="fas fa-graduation-cap"></i></span>
+              </div>
+              <input type="text" className="form-control" onChange={this.handleChange} placeholder="Grade" name="grade" aria-label="Grade" />
+            </div>
+            <div className="btn-box justify-content-end d-flex">
+              <button className="btn-success pb-1" type="submit">Add</button>
+              <button className="btn-success ml-2 pb-1" onClick={this.resetForm} type="reset">Cancel</button>
+            </div>
           </div>
         </form>
       </div >
